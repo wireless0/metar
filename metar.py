14cc5f5d94696c1012dc0dedf7b6349a0dcf8132
Dmitry Sautsa <dsavtsa@gmail.com>
2016-05-04 19:20:23 +0200

    Added metar.py

---
+++ metar.py
@@ -0,0 +1,25 @@
+from bottle import route, run
+import urllib2
+import xmltodict
+
+def get_metar(airfield):
+    if not airfield.isalpha():
+        return "Invalid station string\n"
+    file = urllib2.urlopen('https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={}&hoursBeforeNow=1'.format(airfield))
+    data = file.read()
+    file.close()
+    data = xmltodict.parse(data)
+
+#    return data
+    if data['response']['data']['@num_results'] != '0':
+        return data['response']['data']['METAR'][0]['raw_text']
+    else:
+        return 'No data for {}'.format(airfield)
+
+@route('/')
+@route('/<name>')
+def metar(name = 'AAAA'):
+    return get_metar(name)
+
+run(host='', port=80, debug=True)
+
